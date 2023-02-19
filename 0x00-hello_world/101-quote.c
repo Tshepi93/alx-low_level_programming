45210abbb41bd38b62573da795069a75ed0a8609
Tshepi93 <itshepelengmoholi@gmail.com>
2023-02-19 03:35:22 -0800

    6-size.c updating

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
- * main - A c progame that prints a line the standard error
- * Return 1 (Success)
+/**main - prints " and that pieces of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by newline, to standard error
+ * Return: Alawys 1.
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
